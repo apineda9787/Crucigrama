2f4c6cdf847dad7b3169dc261889dda6accde335
apineda9787 <andrespineda.9704@gmail.com>
2024-03-18 00:50:12 -0500

    Se agrega logica del juego

--- src/co/edu/poli/crucigrama/controller/Main.java
+++ src/co/edu/poli/crucigrama/controller/Main.java
@@ -1,10 +1,26 @@
 package co.edu.poli.crucigrama.controller;
 
+import java.util.Scanner;
+
+import co.edu.poli.crucigrama.model.*;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("crucigrama prueba");
+
+		Logica crucigrama = new Logica();
+		int cerrar = 0;
+
+		while (cerrar == 0) {
+			System.out.println("\nCRUCIGRAMA\n");
+			crucigrama.mostrarCrucigrama();
+			crucigrama.imprimirRespuestas();
+
+			System.out.println("\nIngrese el id de la palabra a validar:");
+			Scanner teclado = new Scanner(System.in);
+			int numero = teclado.nextInt();
+			crucigrama.validarLetra(numero);
+		}
 
 	}
 
--- src/co/edu/poli/crucigrama/model/Logica.java
+++ src/co/edu/poli/crucigrama/model/Logica.java
@@ -1,27 +1,103 @@
 package co.edu.poli.crucigrama.model;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Logica {
 
-	String letras;
-	String Palabras;
-	String pistas;
-	double score;
-	int numeroPalabra;
+	private String[] palabras;
+	private String pistas;
+	private double score;
+	ArrayList<Integer> palabrasMostrar = new ArrayList<Integer>();
 
-	public String getLetras() {
-		return letras;
+	public void obtenerPalabras() {
+		palabras = new String[] { "SOPA", "ARROZ", "PAPA", "YUCA", "MANZANA", "PERA", "PIÑA", "GUANABANA" };
 	}
 
-	public void setLetras(String letras) {
-		this.letras = letras;
+	public void obtenerPistas() {
+		pistas = "Archivo de pistas";
 	}
 
-	public String getPalabras() {
-		return Palabras;
+	public void mostrarCrucigrama() {
+		obtenerPalabras();
+		System.out.println("HORIZONTALES");
+		for (int i = 0; i < palabras.length; i++) {
+			char letras[] = palabras[i].toCharArray();
+			if (i == 4) {
+				System.out.println("\n\nVERTICALES");
+			}
+			System.out.println();
+			for (int j = 0; j < letras.length; j++) {
+				if (palabrasMostrar.size() > 0) {
+					for (int k = 0; k < palabrasMostrar.size(); k++) {
+						if (i == palabrasMostrar.get(k)) {
+							if (j == 0) {
+								System.out.print(i + 1 + "  " + letras[j] + " ");
+							} else {
+								System.out.print("  " + letras[j] + " ");
+							}
+						}
+					}
+				} else {
+					if (j == 0) {
+						System.out.print(i + 1 + "  _ ");
+					} else {
+						System.out.print(" _ ");
+					}
+				}
+			}
+		}
+
 	}
 
-	public void setPalabras(String palabras) {
-		Palabras = palabras;
+	public void imprimirRespuestas() {
+		System.out.println("\n\n");
+		System.out.println("HORIZONTALES");
+		for (int i = 0; i < palabras.length; i++) {
+			if (i == 4) {
+				System.out.println("VERTICALES");
+				System.out.print(i + 1 + " " + palabras[i] + "\n");
+			} else {
+				System.out.print(i + 1 + " " + palabras[i] + "\n");
+			}
+
+		}
+
+	}
+
+	public void validarLetra(int idPalabra) {
+		Scanner teclado = new Scanner(System.in);
+		String palabra = null;
+		for (int i = 0; i < palabras.length; i++) {
+			if (idPalabra - 1 == i) {
+				System.out.println("Ingrese la palabra: ");
+				palabra = teclado.next();
+				if (palabra.equalsIgnoreCase(palabras[i])) {
+					System.out.println("Correcto!");
+					mostrarPalabras(i);
+					sumarScore();
+				} else {
+					System.err.println("Palabra Incorrecta!");
+				}
+			}
+		}
+	}
+
+	public void mostrarPalabras(int idPalabra) {
+		palabrasMostrar.add(idPalabra);
+	}
+
+	public void sumarScore() {
+		score += 10;
+		System.out.println("Su Score es: " + score);
+	}
+
+	public String[] getPalabras() {
+		return palabras;
+	}
+
+	public void setPalabras(String[] palabras) {
+		this.palabras = palabras;
 	}
 
 	public String getPistas() {
@@ -40,12 +116,12 @@ public class Logica {
 		this.score = score;
 	}
 
-	public int getNumeroPalabra() {
-		return numeroPalabra;
+	public ArrayList<Integer> getPalabrasMostrar() {
+		return palabrasMostrar;
 	}
 
-	public void setNumeroPalabra(int numeroPalabra) {
-		this.numeroPalabra = numeroPalabra;
+	public void setPalabrasMostrar(ArrayList<Integer> palabrasMostrar) {
+		this.palabrasMostrar = palabrasMostrar;
 	}
 
 }
